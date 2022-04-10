e604b5f12cdb9802b4bd146eba408ecac2d632fb
Leonard Shearer <leojshe@unc.edu>
2022-04-10 14:37:18 -0700

    Created all functions

--- server.js
+++ server.js
@@ -15,16 +15,30 @@ app.get('/app', (req, res) => {
     res.status(200).end('OK')
 });
 
+app.get('/app/flip', (req, res) => {
+    res.type('application/json')
+    res.status(200).json({ 'flip': coin.coinFlip() })
+});
+
 app.get('/app/flips/:number', (req, res) => {
-    const flips = manyflips(req.params.number)
-    //Other
-    //expressions
-    //go
-    //here
+    const raw = coin.coinFlips(parseInt(req.params.number))
+    const summary = coin.countFlips(raw)
+    res.type('application/json')
+    res.status(200).json({ 'raw': raw, 'summary': summary })
+});
+
+app.get('/app/flip/call/heads', (req, res) => {
+    res.type('application/json')
+    res.status(200).json(coin.flipACoin('heads'))
+});
+
+app.get('/app/flip/call/tails', (req, res) => {
+    res.type('application/json')
+    res.status(200).json(coin.flipACoin('tails'))
 });
 
 app.use(function (req, res) {
-    res.status(404).end('404 NOT FOUND')
     res.type('text/plain')
+    res.status(200).status(404).end('404 NOT FOUND')
 });
 
