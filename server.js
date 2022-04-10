9de877f3b02e1d6ce6bd7d8caa95b65ed1502ea6
Leonard Shearer <leojshe@unc.edu>
2022-04-10 14:12:53 -0700

    Finally fix app endpoint?

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const app = express()
-const minimist = require('minimist')
-const args = minimist(process.argv.slice(2))
+import * as coin from './modules/coin.mjs'
+import express from 'express'
+import minimist from 'minimist'
 
+const app = express()
+const args = minimist(process.argv.slice(2))
 
 const HTTP_PORT = args['port'] || 5000
 const server = app.listen(HTTP_PORT, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
 });
 
-app.get('/app/', (req, res) => {
-    res.statusCode = 200;
-    res.statusMessage = 'OK';
-    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
-    res.status(200).send('OK')
-    res.end(res.statusCode + ' ' + res.statusMessage)
+app.get('/app', (req, res) => {
+    res.type('text/plain')
+    res.status(200).end('OK')
+});
+
+app.get('/app/flips/:number', (req, res) => {
+    const flips = manyflips(req.params.number)
+    //Other
+    //expressions
+    //go
+    //here
 });
 
 app.use(function (req, res) {
